3af7ca3dcb5dc15316d1b5db6bf93ad132aa6201
Emilie Valckenaere <emilie.valckenaere@gmail.com>
2023-12-14 12:48:49 +0100

    vérification crédit utilisateur avant enchère

--- src/main/java/org/projetEncheres/javaee/servlets/EnchereArticleServlet.java
+++ src/main/java/org/projetEncheres/javaee/servlets/EnchereArticleServlet.java
@@ -32,9 +32,11 @@ public class EnchereArticleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		int idArt = Integer.parseInt(request.getParameter("noArt"));
 		ArticleManager amgr = new ArticleManager();
 		Categorie c;
@@ -57,9 +59,11 @@ public class EnchereArticleServlet extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		Utilisateur u = (Utilisateur) session.getAttribute("userCo");
 		Enchere e = null;
@@ -74,34 +78,38 @@ public class EnchereArticleServlet extends HttpServlet {
 			try {
 				a = amgr.selectByID(idArt);
 				int enchereNew = Integer.parseInt(request.getParameter("enchere"));
-				e = new Enchere(u.getNoUtilisateur(), a.getNoArticle(), LocalDate.now(),enchereNew);
-				if(emgr.enchereValide(e)) {
-				emgr.insertEnchere(e, a, u);
-				a.setPrixVente(enchereNew);
-				amgr.update(a);
-				response.addCookie(new Cookie(cookieName, "1"));
-				for (Cookie c : cookies) {
-					if (c.getValue()== "1") {
-						c.setMaxAge(31*24*60*60);
+				if (enchereNew > u.getCredit()) {
+					out.println("<b><font color='red'>Vous ne disposez pas de suffisamment de crédits pour enchérir</font></b>");
+				} else {
+					e = new Enchere(u.getNoUtilisateur(), a.getNoArticle(), LocalDate.now(), enchereNew);
+					if (emgr.enchereValide(e)) {
+						emgr.insertEnchere(e, a, u);
+						a.setPrixVente(enchereNew);
+						amgr.update(a);
+						response.addCookie(new Cookie(cookieName, "1"));
+						for (Cookie c : cookies) {
+							if (c.getValue() == "1") {
+								c.setMaxAge(31 * 24 * 60 * 60);
+							}
+						}
+						RequestDispatcher rd = request.getRequestDispatcher("accueilServlet");
+						rd.forward(request, response);
+
+					} else {
+						out.println(
+								"<b><font color='red'>Le montant de l'enchère ne peut pas être inférieur au prix de l'article</font></b>");
 					}
 				}
-				RequestDispatcher rd = request.getRequestDispatcher("accueilServlet");
-				rd.forward(request, response);
-				} else {
-					out.println("<b><font color='red'>Le montant de l'enchère ne peut pas être inférieur au prix de l'article</font></b>");
-				}
-				
+
 			} catch (DALException e1) {
 				e1.printStackTrace();
 			} catch (BLLException e1) {
 				e1.printStackTrace();
 			}
 		}
-		
-		
-		
+
 	}
-	
+
 	private Utilisateur getVendeur(ArticleVendu a) {
 		Utilisateur u2 = null;
 		UtilisateurManager umgr = new UtilisateurManager();
@@ -115,7 +123,7 @@ public class EnchereArticleServlet extends HttpServlet {
 		}
 		return u2;
 	}
-	
+
 	private Categorie getCategorie(ArticleVendu a) {
 		CategorieManager cmgr = new CategorieManager();
 		Categorie c = null;
@@ -124,10 +132,8 @@ public class EnchereArticleServlet extends HttpServlet {
 		} catch (DALException | BLLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return c;
 	}
-	
-	
 
 }
